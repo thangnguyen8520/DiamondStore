b60ec9cf3c77a383d2c1fafb276c8800f0cf8169
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-06-20 08:03:33 +0700

    GetUser

---
+++ DiamondStoreRepository/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using DiamondDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondStoreRepository
+{
+    public class UserRepository
+    {
+        private readonly UserDAO userDAO = null;
+        public UserRepository()
+        {
+            if (accountDAO == null)
+            {
+                accountDAO = new AccountDAO();
+            }
+        }
+        public Account GetAccount(string userName, string password) => AccountDAO.Instance.GetAccount(userName, password);
+        //{
+        //    return accountDAO.GetAccount(userName, password);
+        //}
+    }
+}
