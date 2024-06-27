c1b808e6de633b6b1968b43ff069fa166907e4b2
dattgt <trangiathanhdat02092002@gmail.com>
2024-06-27 22:19:09 +0700

    create new Cards Admin UI

---
+++ DiamondStore/Pages/Admin/Cards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiamondStore.Pages.Admin
+{
+    public class CardsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
