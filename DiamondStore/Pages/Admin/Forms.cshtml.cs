90cf029b89f038f8e86437feccb470e3ecb65ee5
dattgt <trangiathanhdat02092002@gmail.com>
2024-06-27 22:19:46 +0700

    create Forms Admin UI

---
+++ DiamondStore/Pages/Admin/Forms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiamondStore.Pages.Admin
+{
+    public class FormsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
