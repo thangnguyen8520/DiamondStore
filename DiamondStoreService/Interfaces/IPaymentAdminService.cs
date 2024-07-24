db17918b22a6b015d22305b30ff9aec08c564fc2
dattgt <trangiathanhdat02092002@gmail.com>
2024-07-24 22:13:24 +0700

    update and add new payment admin Service, dto

---
+++ DiamondStoreService/Interfaces/IPaymentAdminService.cs
@@ -0,0 +1,15 @@
+﻿using DiamondStoreService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondStoreService.Interfaces
+{
+    public interface IPaymentAdminService
+    {
+        Task<IEnumerable<PaymentDTO>> GetAllPaymentsAsync();
+
+    }
+}
